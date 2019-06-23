367d94a54636af854f6540e960e7b7bc05da9127
rbespin <rbespino@ucsd.edu>
2019-06-23 14:30:38 -0700

    brainstorm

--- summarizer.cpp
+++ summarizer.cpp
@@ -13,11 +13,11 @@ using namespace std;
 int main(int argc, char * argv[]){
 
 
-   if(argc != 3){
-	  cout << "This program requires a text file argument and an output file"
-		 << endl;
-	  return 0;
-   }
+//   if(argc != 3){
+//	  cout << "This program requires a text file argument and an output file"
+//		 << endl;
+//	  return 0;
+//   }
 
    /* Empty file edge case */
    ifstream efile;
@@ -50,6 +50,23 @@ int main(int argc, char * argv[]){
    outfile.close();
    file.close();
 
+   if(argv[3] != NULL){
+	  ifstream file;
+	  file.open(argv[1], ios::binary);
+
+	  ofstream testout;
+	  testout.open(argv[3]);
+	  
+	  unsigned int character;
+
+	  while( (character = file.get()) != EOF ){
+		 if( (unsigned char)character == 'a' ){
+			continue;
+		 }
+		 testout << (unsigned char)character;
+	  }
+   }
+
    return 0;
 }
 
@@ -60,5 +77,15 @@ int main(int argc, char * argv[]){
 *  frequencies
 * 
 *  - Function to separate all sentences of a file and place them in a set
+*	  - How to separate sentences? have cases when we encounter a character
+*	  not within ASCII value range 33-127.
+*	  - Have cases when we encounter sentence ending punctuation, like '.',
+*	  '!', '?', ')' (perhaps if we've read a '(' beforehand), same for ']',
+*	  bookend 'newlines'?
+*	  - Periods followed by an uppercase character or number. IGNORE numbers
+*	  immediately after a decimal point. Capital letters after a period
+*	  signify start of new sentences. But also need to account for factorial
+*	  numbers. (4!). Should functions work based on the fact that perfect
+*	  sentences may not be able to be extracted?
 *
 */
